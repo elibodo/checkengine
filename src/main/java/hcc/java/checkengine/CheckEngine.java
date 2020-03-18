6438d17edc5039211f9f5d92400c93a8184122d0
Zachary Fry <zacharydf7@gmail.com>
2020-03-17 20:32:49 -0400

    Added eclipse project files to gitignore.

--- src/main/java/hcc/java/checkengine/CheckEngine.java
+++ src/main/java/hcc/java/checkengine/CheckEngine.java
@@ -2,6 +2,7 @@ package hcc.java.checkengine;
 
 public class CheckEngine {
     public static void main(String[] args) {
-        SearchFrame frame = new SearchFrame();
+        //Main entry point.
+    	SearchFrame frame = new SearchFrame();
     }
 }
